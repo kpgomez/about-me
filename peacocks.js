97bd7528a573868ac397655e873aa0414b499ce5
Kiengchay Gomez <kphoutthavong@gmail.com>
2023-01-22 01:01:13 -0600

    added prompts

--- peacocks.js
+++ peacocks.js
@@ -1,3 +1,12 @@
-let answer = prompt("Greetings, what shall I call you?");
-document.write("Hi " + answer + ", aren't peafowls just magnificent?")
+let name = prompt("Greetings, what shall I call you?");
+console.log("Well " + name + " you have a magnificent name");
 
+let query = prompt("Would you like to help by donate to conservation efforts??");
+let message;
+
+if (query=='yes') {
+    message = "Your generosity is much appreciated!";
+} else {
+    message = "Please reconsider. A small donation will go a long way!"
+}
+    
