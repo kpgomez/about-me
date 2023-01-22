f559fbc94a48e59ed47bb2ec0839b702813575cb
Kiengchay Gomez <kphoutthavong@gmail.com>
2023-01-22 01:16:27 -0600

    corrected output issues

--- peacocks.js
+++ peacocks.js
@@ -1,12 +1,12 @@
-let name = prompt("Greetings, what shall I call you?");
-console.log("Well " + name + " you have a magnificent name");
+let visitor = prompt("Greetings, what shall I call you?");
+document.write("Well " + visitor + ", you have a magnificent name! ");
 
-let query = prompt("Would you like to help by donate to conservation efforts??");
+let query = prompt("Would you like to help by donating to conservation efforts??");
 let message;
 
 if (query=='yes') {
     message = "Your generosity is much appreciated!";
 } else {
-    message = "Please reconsider. A small donation will go a long way!"
+    message = "Please reconsider. A small donation will go a long way to preserve this magnificent creature!"
 }
-    
+document.write(message);
