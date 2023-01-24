d9ad1302574956a26db5517351a4fd6b3b1f7fa5
Kiengchay Gomez <kphoutthavong@gmail.com>
2023-01-24 00:27:11 -0600

    converted inline code to functions

---
+++ app.js
@@ -0,0 +1,18 @@
+function greeting(){
+    let name = prompt("Greetings, what shall I call you?");
+    document.write("Hello " + name + "!");
+}
+
+function askForDonation(){
+    let query = prompt("Would you like to donate to conservation efforts?");
+    let message; 
+    
+    if (query=='yes') {
+        message = ("Your generosity is much appreciated!");
+} else {
+    message = "Please reconsider. A small donation goes a long way!"
+}
+
+document.write(message);
+
+}
